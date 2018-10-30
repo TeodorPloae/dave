8c216887e77a530a8a916c1c178b72fb37a3ee1b
anggabard <angel.ardeleanu@hotmail.com>
2018-10-30 17:08:52 +0200

    form refactor and javascript added

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
  
-var elemente = document.querySelectorAll('.tab a')
+var elemente = document.querySelectorAll('.tab');
+var sign_up_button = elemente[0];
+var login_button = elemente[1];
+
 var login = document.getElementById("login");
 var sign_up = document.getElementById("sign-up");
+
+var main_form = document.getElementById("main_form");
+var sign_up_list_element = document.getElementById("sign_up_element");
+var login_list_element = document.getElementById("login_element");
 login.style.display = 'none';
 
-elemente[0].addEventListener('click', function (e) {
-
-    sign_up.style.display = 'inline-grid';
+sign_up_button.addEventListener('click', function (e) {
+    sign_up.style.display = 'grid';
     login.style.display = 'none';
-    
+
+    sign_up_list_element.classList.remove("inactive");
+    login_list_element.classList.add("inactive");
 });
 
-elemente[1].addEventListener('click', function (e) {
+login_button.addEventListener('click', function (e) {
     
-    login.style.display = 'inline-grid';
+    login.style.display = 'grid';
     sign_up.style.display = 'none';
-    
+
+    login_list_element.classList.remove("inactive");
+    sign_up_list_element.classList.add("inactive");
 });
