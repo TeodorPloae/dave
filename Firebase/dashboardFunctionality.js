bc5ff2aeacb517a93bceb40d44937daa9e9f6ac9
anggbard <gabrielangel.ardeleanu@gmail.com>
2019-01-28 23:04:05 +0200

    started getUserData from Owner uid

--- Firebase/dashboardFunctionality.js
+++ Firebase/dashboardFunctionality.js
@@ -4,51 +4,54 @@ const link = document.getElementById("download_link");
 const db_select = document.getElementById("DB_Select");
 const modal = document.getElementById("modal");
 const site_name = document.getElementById("site_name");// not in html atm
-
+var readDownload = true;
 var storage = firebase.storage();
 var pathReference = storage.ref('thefile.js');
 
+const enc = new TextEncoder();
+
 var srvPublicKey;
 
-    var req = new XMLHttpRequest();
+var req = new XMLHttpRequest();
 
-    // Feature detection for CORS
-    if ('withCredentials' in req) {
-        req.open('GET', 'http://localhost:5016/getPublicKey', true);
-        // Just like regular ol' XHR
-        req.onreadystatechange = function () {
-            if (req.readyState === 4) {
-                if (req.status >= 200 && req.status < 400) {
-                    // JSON.parse(req.responseText) etc.
-                    var JsonResponse = JSON.parse(req.response);
-                    
-                    window.crypto.subtle.importKey(
-                        "jwk",
-                        JsonResponse,
-                        {
-                            name: "RSA-OAEP",
-                            modulusLength: 1024,
-                            publicExponent: new Uint8Array([1, 0, 1]),
-                            hash: {
-                              name: "SHA-1"
-                            }
+// Feature detection for CORS
+if ('withCredentials' in req) {
+    req.open('GET', 'http://localhost:5016/getPublicKey', true);
+    // Just like regular ol' XHR
+    req.onreadystatechange = function () {
+        if (req.readyState === 4) {
+            if (req.status >= 200 && req.status < 400) {
+                // JSON.parse(req.responseText) etc.
+                var JsonResponse = JSON.parse(req.response);
+
+                window.crypto.subtle.importKey(
+                    "jwk",
+                    JsonResponse,
+                    {
+                        name: "RSA-OAEP",
+                        modulusLength: 1024,
+                        publicExponent: new Uint8Array([1, 0, 1]),
+                        hash: {
+                            name: "SHA-1"
                         }
-                            ,
-                        false,
-                        ["encrypt"]
-                    )
-                    .then( function(key) {
+                    }
+                    ,
+                    false,
+                    ["encrypt"]
+                )
+                    .then(function (key) {
                         srvPublicKey = key;
                     });
 
-                } else {
-                    // Handle error case
-                    console.log("There was an error at GetPublicKey from server!");
-                }
+            } else {
+                // Handle error case
+                link.innerHTML = "Server is down!";
+                readDownload = false;
             }
-        };
-        req.send();
-    }
+        }
+    };
+    req.send();
+}
 
 logout_button.addEventListener('click', e => {
     firebase.auth().signOut()
@@ -60,8 +63,10 @@ logout_button.addEventListener('click', e => {
         .catch(e => console.log(e.message));
 });
 
-
 function loadData(theUser) {
+    if (!readDownload) {
+        return;
+    }
     storage.ref('thefile.js').getDownloadURL()
         .then(function (url) {
             link.setAttribute('download', 'thefile.js');
@@ -75,14 +80,12 @@ function loadData(theUser) {
                 const reader = new FileReader();
 
                 reader.addEventListener('loadend', (e) => {
-                    
+
                     var text = e.srcElement.result;
 
                     text = text.replace("{RSAPublicKey}", JSON.stringify(theUser.publicKey));
 
                     link.setAttribute('download', 'theFile.js');
-
-                    var enc = new TextEncoder();
 
                     window.crypto.subtle.encrypt(
                         {
@@ -90,25 +93,27 @@ function loadData(theUser) {
                             modulusLength: 1024,
                             publicExponent: new Uint8Array([1, 0, 1]),
                             hash: {
-                              name: "SHA-1"
+                                name: "SHA-1"
                             }
                         },
                         srvPublicKey,
                         enc.encode(theUser.uid)
                     )
-                    .then( function (encryptedUid) {
-                        let encryptedArray = new Uint8Array(encryptedUid);
+                        .then(function (encryptedUid) {
+                            theUser.encryptedUid = new Uint8Array(encryptedUid)
 
-                        text = text.replace("{uid}", JSON.stringify(encryptedArray));
-                        link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-                        
-                        download_button.disabled = false;
-                        download_button.style.backgroundColor = "#1ab188";
-                        requireSiteName();
-                        
-                    })
-                    .catch(e => console.log(e.message));
-                    
+                            text = text.replace("{uid}", JSON.stringify(theUser.encryptedUid));
+                            link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+
+
+                            download_button.disabled = false;
+
+                            download_button.style.backgroundColor = "#1ab188";
+                            requireSiteName();
+                            getUserData(theUser);
+                        })
+                        .catch(e => console.log(e.message));
+
                 });
 
                 reader.readAsText(blob);
@@ -132,7 +137,33 @@ window.onclick = function (event) {
     }
 }
 
-function requireSiteName(){
+function requireSiteName() {
     //to be implemented
     console.log("reminder! Implement site name field!");
+}
+
+function getUserData(theUser) {
+    var request = new XMLHttpRequest();
+    if ('withCredentials' in request) {
+        request.open('POST', 'http://localhost:5016/getUserDataFromSiteName', true);
+        // Just like regular ol' XHR
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 400) {
+                    // JSON.parse(request.responseText) etc.
+                    var JsonResponse = JSON.parse(request.response);
+
+                    console.log(JsonResponse);
+                } else {
+                    // Handle error case
+                    console.log("error ")
+                }
+            }
+        };
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        let data = JSON.stringify({"uid" : theUser.encryptedUid, "siteName" : "jetix"});
+        console.log(data);
+        request.send(data);
+    }
+
 }
--- Server Test/server.js
+++ Server Test/server.js
@@ -16,76 +16,76 @@ var webcrypto = new WebCrypto();
 var textEncoding = require('text-encoding');
 var TextDecoder = textEncoding.TextDecoder;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({ type: 'text/plain'}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'text/plain' }))
 
 var publicKey;
 var privateKey;
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 firebase.database().ref('/server/').once('value')
-            .then( function (snapshot) {
-                webcrypto.subtle.importKey(
-                    "jwk",
-                    snapshot.val().privateKey,
-                    {
-                        name: "RSA-OAEP",
-                        modulusLength: 1024,
-                        publicExponent: new Uint8Array([1, 0, 1]),
-                        hash: {
-                          name: "SHA-1"
-                        }
-                    },
-                    false,
-                    ["decrypt"]
-                    )
-                    .then( (key) => {
-                        privateKey = key;
-                    }) 
+    .then(function (snapshot) {
+        webcrypto.subtle.importKey(
+            "jwk",
+            snapshot.val().privateKey,
+            {
+                name: "RSA-OAEP",
+                modulusLength: 1024,
+                publicExponent: new Uint8Array([1, 0, 1]),
+                hash: {
+                    name: "SHA-1"
+                }
+            },
+            false,
+            ["decrypt"]
+        )
+            .then((key) => {
+                privateKey = key;
+            })
 
-                publicKey = snapshot.val().publicKey;
-            });
+        publicKey = snapshot.val().publicKey;
+    });
 
 app.post('/', function (req, res) {
     let uidArray = uidJsonToArray(req.body.ownerData.uid);
 
-    if (req.body.ownerData.uid){
+    if (req.body.ownerData.uid) {
         webcrypto.subtle.decrypt(
             {
                 name: "RSA-OAEP",
                 modulusLength: 1024,
                 publicExponent: new Uint8Array([1, 0, 1]),
                 hash: {
-                  name: "SHA-1"
+                    name: "SHA-1"
                 }
             },
             privateKey,
             uidArray
         )
-        .then((decrypted) => {
-           var decryptedUid = new TextDecoder("utf-8").decode(new Uint8Array (decrypted));
+            .then((decrypted) => {
+                var decryptedUid = new TextDecoder("utf-8").decode(new Uint8Array(decrypted));
 
-           firebase.database().ref('users/' + decryptedUid + '/sites/' + req.body.ownerData.siteName).set(
-                req.body.userData
-           )
-           .catch(e => console.log(e.message));
-        })
-        .catch(e => console.log(e.message));
+                firebase.database().ref('users/' + decryptedUid + '/sites/' + req.body.ownerData.siteName).set(
+                    req.body.userData
+                )
+                    .catch(e => console.log(e.message));
+            })
+            .catch(e => console.log(e.message));
     } else {
         res.send(500);
     }
 });
 
 app.get('/getPublicKey', function (req, res) {
-    
+
     console.log("am primit cv de la dashboard");
     res.json(publicKey);
     console.log("am trimis cv la dashboard");
-    
+
 });
 
 app.listen(5016, function () {
@@ -108,3 +108,39 @@ function uidJsonToArray(json) {
 //       name: "SHA-1"
 //     }
 //   }
+
+app.post('/getUserDataFromSiteName', function (req, res) {
+
+console.log(req);
+
+    // let uidArray = uidJsonToArray(req.body.uid);
+    // let siteName = req.body.siteName;
+
+    // if (uidArray) {
+    //     webcrypto.subtle.decrypt(
+    //         {
+    //             name: "RSA-OAEP",
+    //             modulusLength: 1024,
+    //             publicExponent: new Uint8Array([1, 0, 1]),
+    //             hash: {
+    //                 name: "SHA-1"
+    //             }
+    //         },
+    //         privateKey,
+    //         uidArray
+    //     )
+    //         .then((decrypted) => {
+    //             var decryptedUid = new TextDecoder("utf-8").decode(new Uint8Array(decrypted));
+
+    //             firebase.database().ref('users/' + decryptedUid + '/sites/' + siteName).once('value')
+    //                 .then(function (snapshot) {
+    //                     console.log(snapshot);
+    //                     res.json(snapshot);
+    //                 })
+    //                 .catch(e => console.log(e.message));
+    //         })
+    //         .catch(e => console.log(e.message));
+    // } else {
+    //     res.send(500);
+    // }
+});
